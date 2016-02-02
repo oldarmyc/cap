bdfc23a99edafc40d15d9b10ef1cae621070d747
davi4261 <david.kludt@rackspace.com>
2016-02-02 15:21:57 -0600

    Update dockerfile and runapp ports

--- runapp.py
+++ runapp.py
@@ -4,4 +4,4 @@ from cap.setup_application import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5006, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
